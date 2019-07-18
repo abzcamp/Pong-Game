98e80ad5cc6cd512cfe12cc05a3cbb1e27179729
Abzcamp <abzcamp@gmail.com>
2019-07-18 09:47:55 -0400

    feat: pause function added

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,10 @@
-import {SVG_NS} from '../settings.js'
+import {SVG_NS, KEYS} from '../settings.js'
+
+import Board from './Board.js';
+
+import Paddle from './Paddle.js'
+
+import Ball from './Ball.js'
 
 export default class Game {
   constructor(element, width, height) {
@@ -6,10 +12,70 @@ export default class Game {
     this.width = width;
     this.height = height;
 
-		this.gameElement = document.getElementById(this.element)
-  }
+     this.gameElement = document.getElementById(this.element)
+    this.board = new Board(this.width, this.height)
 
+    this.paddleWidth = 8
+    this.paddleHeight = 56
+    this.boardGap = 10
+
+    this.board = new Board(this.width, this.height)
+    this.Ball = new Ball(8, this.width, this.height)
+
+    //player one
+    this.player1 = new Paddle(
+      'yellow',
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.a,
+      KEYS.z
+
+      )
+
+    //player two
+    this.player2 = new Paddle(
+      'blue',
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      (this.width - this.boardGap - this.paddleWidth),
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.up,
+      KEYS.down
+      
+
+    )
+
+
+    this.gameElement = document.getElementById(this.element)
+    
+
+document.addEventListener('keydown', event => {
+
+  switch(event.key) {
+    case KEYS.spaceBar:
+      this.pause = !this.pause
+      this.player1.speed = 10
+      this.player2.speed = 10
+      
+   
+
+}
+
+
+
+  })
+  }
   render() {
+
+    if(this.pause){
+      this.player1.speed = 0
+      this.player2.speed = 0
+      return
+    }
     //clear board
     this.gameElement.innerHTML = ''
     //create SVG element for the board
@@ -19,7 +85,14 @@ export default class Game {
     svg.setAttributeNS(null, 'width', this.height)
     svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`)
     this.gameElement.appendChild(svg)
-    
 
-  }
+    this.board.render(svg)
+    this.player1.render(svg)
+    this.player2.render(svg)
+    this.Ball.render(svg)
+
+
+
+  
+}
 }
---
+++ src/partials/Paddle.js
@@ -0,0 +1,40 @@
+import {SVG_NS} from '../settings.js'
+
+export default class Paddle {
+
+    constructor(color, boardHeight, width, height, x, y, up, down){
+        this.boardHeight = boardHeight
+        this.width = width
+        this.height = height
+        this.x = x
+        this.y = y
+        this.speed = 10
+        this.score = 0
+        this.color = color
+        let that = this
+
+        document.addEventListener('keydown', function(event){
+            switch(event.key) {
+                case up:
+                    that.y = Math.max(0, that.y - that.speed)
+                break;
+                case down:
+                    that.y = Math.min(that.boardHeight - that.height, that.y + that.speed)
+                break
+            }
+        })
+    }
+
+    render(svg) {
+        let rect = document.createElementNS(SVG_NS, 'rect')
+        rect.setAttributeNS(null, 'fill', this.color)
+        rect.setAttributeNS(null, 'width', this.width)
+        rect.setAttributeNS(null, 'height', this.height)
+        rect.setAttributeNS(null, 'rx', '5')
+        rect.setAttributeNS(null, 'x', this.x)
+        rect.setAttributeNS(null, 'y', this.y)
+
+        svg.appendChild(rect);
+    }
+
+}
